45287ee3d9ce9eb07c7148b6bb19cdeb33c19756
SonDN <sondoannam202@gmail.com>
2024-10-18 11:37:47 +0700

    build(deps): :rocket: setup prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UsersModule } from './users/users.module';
+
+import { DatabaseModule } from './database/database.module';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
