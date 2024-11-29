8700e684266ea6a865e6f24ca8aaa145e0498074
SonDN <sondoannam202@gmail.com>
2024-11-30 02:28:42 +0700

    fix(deps): :bug: trying to fix build errors...

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,21 +1,10 @@
-import { Request } from 'express';
-import { Multer } from 'multer';
+/// <reference types="express" />
+/// <reference types="multer" />
 
 declare global {
   namespace Express {
-    interface Request extends Request {}
-    namespace Multer {
-      interface File {
-        fieldname: string;
-        originalname: string;
-        encoding: string;
-        mimetype: string;
-        size: number;
-        destination: string;
-        filename: string;
-        path: string;
-        buffer: Buffer;
-      }
-    }
+    // Express namespace is already defined in @types/express
   }
 }
+
+export {};
