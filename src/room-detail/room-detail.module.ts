a9877ed2f403df350726c772c8d12bbc1917eb43
SonDN <sondoannam202@gmail.com>
2024-12-18 06:00:37 +0700

    chore: update module

--- src/room-detail/room-detail.module.ts
+++ src/room-detail/room-detail.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoomDetailService } from './room-detail.service';
 import { RoomDetailController } from './room-detail.controller';
+import { DatabaseModule } from '@/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [RoomDetailController],
   providers: [RoomDetailService],
 })
