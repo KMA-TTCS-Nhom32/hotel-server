97fb708197e30999e61b0f59b443cd523c0c6796
SonDN <sondoannam202@gmail.com>
2025-01-09 00:56:39 +0700

    fix(app): :bug: fix missing exports

--- src/modules/room-detail/room-detail.module.ts
+++ src/modules/room-detail/room-detail.module.ts
@@ -7,5 +7,6 @@ import { DatabaseModule } from '@/database/database.module';
   imports: [DatabaseModule],
   controllers: [RoomDetailController],
   providers: [RoomDetailService],
+  exports: [RoomDetailService],
 })
 export class RoomDetailModule {}
