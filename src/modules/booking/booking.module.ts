443aaa6759a40fa3893270dfbbb88bf4f63e507f
SonDN <sondoannam202@gmail.com>
2025-01-09 00:51:17 +0700

    fix(app): :bug: fix import

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { DatabaseModule } from '@/database/database.module';
-import { RoomDetailModule } from '../room-detail/room-detail.module';
+import { RoomDetailModule } from '@/modules/room-detail/room-detail.module';
 
 @Module({
   imports: [DatabaseModule, RoomDetailModule],
